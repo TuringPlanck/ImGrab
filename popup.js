1de4beefb48ab7a0c665eae438df52a0df0d9c1b
Logan Martel <logan.martel@outlook.com>
2015-01-17 17:05:51 -0500

    download native format

--- popup.js
+++ popup.js
@@ -6,29 +6,48 @@ chrome.extension.onMessage.addListener(function(request, sender) {
   }
 });
 
-function getImages(HTML) {
+function getImages(Page) {
   
   // extracts page images to popup
   var imgCounter = 0;
-  $('.UFICommentContentBlock', HTML).find('a[rel="theater"]').find("img").each( function() {
+  $('.UFICommentContentBlock', Page).find('a[rel="theater"]').each( function() {
       
       var zoom = document.createElement('a');
       document.body.appendChild(zoom);
 
       // adds lightbox to images
       var img = document.createElement('img');
-      img.src = $(this).attr('src');
+      img.src = $(this).find("img").attr('src');
       imgCounter++;
       zoom.innerHTML = '<a href="' + img.src + '"data-lightbox="Image">' + img.outerHTML + '</a>';
+
+      // gets download link for image
+      var downloader = getDownloader($(this).attr('href'));
 
       // adds download button to images
       var button = document.createElement('button');
       button.type = 'download';
       button.appendChild(document.createTextNode('Download'));
       document.body.appendChild(button);
-      button.innerHTML = '<a href="' + img.src + '"download="' + 'Img' + imgCounter + '.jpg' + '">Download</a>';
+      button.innerHTML = '<a href="' + downloader + '"download="' + 'Img' + imgCounter + '.jpg' + '">Download</a>';
     });
   }
+
+function getDownloader(url) {
+  var result = null;
+     $.ajax({
+        url: url,
+        type: 'get',
+        dataType: 'html',
+        async: false,
+        success: function(data) {
+            result = data;
+        }
+     });
+     var HTML = $.parseHTML(result);
+     var downloader = $('.fbPhotoImage', HTML).attr('src');
+     return downloader;
+}
 
 function onWindowLoad() {
 
