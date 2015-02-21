5a0a799cd5fe504d827306b33df8b6e47dbb3cfb
Logan Martel <logan.martel@outlook.com>
2015-02-21 01:16:40 -0500

    drag and drop download

--- js/window.js
+++ js/window.js
@@ -1,24 +1,40 @@
 function onWindowLoad() {
 	chrome.storage.sync.get(null, function(result){
-		var imgCounter = 0;
-		for(i = 0; i < result["src"].length; i++) {
+		for(i = 0; i < result['src'].length; i++) {
 
 		  var image = document.createElement('a');
 	      var display = document.createElement('img');
-	      imgCounter++;
 
 	      // add image to window
 	      document.body.appendChild(image);
-	      display.src = result["src"][i];
+	      display.src = result['src'][i];
 	      image.outerHTML = '<a href="' + display.src + '"data-lightbox="Image">' + display.outerHTML + '</a>';
-
-	      // add download button to images
-	      var button = document.createElement('button');
-	      button.type = 'download';
-	      document.body.appendChild(button);
-	      button.innerHTML = '<a href="' + display.src + '"download="' + 'Img' + imgCounter + '.jpg' + '">Download</a>';
 		}
 	});
+	var dropZone = document.getElementById('dropZone');
+	dropZone.addEventListener('dragover', handleDragOver, false);
+	dropZone.addEventListener('drop', drop, false);
+	$('#downloadAll').click(function() {
+      $('a.selected > img').each(function() {
+        $(this).trigger( "click" );
+      });
+  	});
+}
+
+function handleDragOver(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+	evt.dataTransfer.dropEffect = 'copy'; 
+}
+
+var imgCounter = 0;
+function drop(evt) {
+	evt.stopPropagation();
+	evt.preventDefault(); 
+	imgCounter++;
+	var imageUrl = evt.dataTransfer.getData('URL');
+	var downloader = '<a href="' + imageUrl + '"download="' + 'Img' + imgCounter + '.jpg' + '" class="selected"><img src="' + imageUrl + '"></a>';
+	$('#images').append(downloader);
 }
 
 window.onload = onWindowLoad;
